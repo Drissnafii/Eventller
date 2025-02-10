850000cd72246a17a72e5e4c1e8633b93353e7ed
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-02-10 10:24:47 +0100

    feat:adding midatabase migration

---
+++ migrations/003_create_tickets_table.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE tickets {
+    id SERIAL PRIMARY KEY,
+    eventId INT,
+    userId INT,
+    FOREIGN KEY (eventId) REFERENCES events(id) ON DELETE CASCADE,
+    FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
+}
