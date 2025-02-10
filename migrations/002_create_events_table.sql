b3ea5e0e58131b0d64fbcdd7ea057e8c5fe24143
9asdaoui <oussamaqasdaoui@gmail.com>
2025-02-10 12:03:03 +0100

    fix: update event table (add places, cotegory_id of the event)

--- migrations/002_create_events_table.sql
+++ migrations/002_create_events_table.sql
@@ -7,6 +7,9 @@ CREATE TABLE events {
     image TEXT DEFAULT '',
     userId INT,
     eventId INT,
+    category_id int , 
+    places int, 
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (userId) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (eventId) REFERENCES events(id) ON UPDATE CASCADE ON DELETE SET NULL
 }
