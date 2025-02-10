05e1c1bdb7e4eaba38484592a7ec6d1692f2ebbd
Yassine El hassani <yassine.ds.404@gmail.com>
2025-02-10 16:19:54 +0100

    adding user model class and implement of the signup func & edit some new stuf in database

--- migrations/002_create_events_table.sql
+++ migrations/002_create_events_table.sql
@@ -1,12 +1,12 @@
-CREATE TABLE events {
+CREATE TABLE events (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     location VARCHAR(255) NOT NULL,
-    datetime DATETIME NOT NULL,
+    datetime TIMESTAMP NOT NULL,
     image TEXT DEFAULT '',
     userId INT,
     eventId INT,
     FOREIGN KEY (userId) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (eventId) REFERENCES events(id) ON UPDATE CASCADE ON DELETE SET NULL
-}
+);
