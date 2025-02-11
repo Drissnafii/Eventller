b49d7357ec24363b9986b448d75690b8080a39ab
Yassine El hassani <yassine.ds.404@gmail.com>
2025-02-11 09:22:17 +0100

    update the last changes

---
+++ src/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+
+
+class UserController {
+    private $userModel;
+
+    public function __construct() {
+        // $this->userModel = new User();
+    }
+
+    
+    
+}
+
+
+?>
--- src/Models/User.php
+++ src/Models/User.php
@@ -19,6 +19,7 @@ class User {
 
         $this->id = $id;
         $this->name = $name;
+        $this->email = $email;
         $this->password = $password;
         $this->$avatar = $avatar;
         $this->role = $role;
