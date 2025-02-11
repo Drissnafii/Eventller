1e12b31bb57910c7bd438aa374e992f5f930a4d4
9asdaoui <oussamaqasdaoui@gmail.com>
2025-02-11 09:18:34 +0100

    small change

---
+++ src/Controllers/ContollerTemplate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\TwigController;
+
+class ContollerTemplate extends TwigController{
+    
+    public function Home(){
+        echo $this->twig->render('client/home.twig', []);
+    }
+    public function Signin(){
+        echo $this->twig->render('client/signin.twig', []);
+    }
+    public function Signup(){
+        echo $this->twig->render('client/signup.twig', []);
+    }
+    public function Forgotpassword(){
+        echo $this->twig->render('client/forgotpassword.twig', []);
+    }
+    public function Dashboard(){
+        echo $this->twig->render('admin/dashborad.twig', []);
+    }
+}
