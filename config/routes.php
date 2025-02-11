58439244ef978e9b09d4c6dbcc5adbdc05036154
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-02-11 09:35:51 +0100

    feat: adding events page

--- config/routes.php
+++ config/routes.php
@@ -13,6 +13,7 @@ class Routes{
             'GET' => [
                 new Route(uri:'/', contoller:ContollerTemplate::class ,method:'Home'),
                 new Route(uri:'/dashboard', contoller:ContollerTemplate::class,method:'Dashboard'),
+                new Route(uri:'/events', contoller:ContollerTemplate::class,method:'Events'),
                 new Route(uri:'/signin',contoller:ContollerTemplate::class,method:'Signin'),
                 new Route(uri:'/signup',contoller:ContollerTemplate::class,method:'Signup'),
                 new Route(uri:'/forgotpassword',contoller:ContollerTemplate::class,method:'Forgotpassword')
