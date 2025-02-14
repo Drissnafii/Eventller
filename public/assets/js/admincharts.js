7ebbfbf209ebc85265a994d7d6a841b558c60dc3
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-02-14 14:50:19 +0100

    adding card dahsborad admin display

--- public/assets/js/admincharts.js
+++ public/assets/js/admincharts.js
@@ -18,50 +18,39 @@ async function fetchdata(){
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                     </svg>
                 </div>` }
-               
-                {% elseif  card.title == 'Active Users' %}
-                 <div class="bg-pink-600/20 p-2 rounded-lg">
+                ${item.title == 'Active Users'  && `<div class="bg-pink-600/20 p-2 rounded-lg">
                     <svg class="w-5 h-5 text-pink-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
-                </div>
-                {% elseif  card.title == 'Total Revenue' %}
-                     <div class="bg-green-600/20 p-2 rounded-lg">
+                </div>` }
+                ${item.title == 'Total Revenue'  && `<div class="bg-green-600/20 p-2 rounded-lg">
                     <svg class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
-                </div>
-                {% elseif  card.title == 'Total Events' %}
-                 <div class="bg-blue-600/20 p-2 rounded-lg">
+                </div>` }
+                  ${item.title == 'Total Events'  && `<div class="bg-blue-600/20 p-2 rounded-lg">
                     <svg class="w-5 h-5 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"></path>
                     </svg>
-                </div>
-                {% else %}
-                <div></div>
-                {% endif %}
-            </div>
-            <p class="text-3xl font-bold mt-4">{{card.value}}</p>
-                {% if card.etat == 'up' %}
-                        <p class="text-green-400 text-sm font-medium mt-2 flex items-center">
+                </div>`}
+            <p class="text-3xl font-bold mt-4">${item.value}</p>
+                ${item.etat == 'up' ?
+                `<p class="text-green-400 text-sm font-medium mt-2 flex items-center">
                                 <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path>
                                 </svg>
                                 +{{card.percentage}}% from last month
-                        </p>
-
-                {% else %}
-                    <p class="text-red-400 text-sm font-medium mt-2 flex items-center">
+                        </p>`:
+                `
+                <p class="text-red-400 text-sm font-medium mt-2 flex items-center">
                         <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"></path>
                         </svg>
                         -{{card.percentage}}% from last month
                     </p>
-                {% endif %}
-
+                `
+                }
         </div>
         `;
     });
-                        
-
 }
