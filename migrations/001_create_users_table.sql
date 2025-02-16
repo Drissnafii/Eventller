16fb9078403abb4a7577e1616a1553aa60648170
drissnafii <drissnafi3@gmail.com>
2025-02-16 17:58:18 +0100

    Refactor database migrations: drop existing database, recreate tables for users, events, tickets, categories, and booking

--- migrations/001_create_users_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    avatar TEXT DEFAULT '',
-    role VARCHAR(20) CHECK (role IN ('admin', 'user', 'organisator')),
-    isActivated BOOLEAN NOT NULL DEFAULT FALSE
-);
